7096fb74a406a959e010e445ef0395426908ad91
Maya Lisitskaya <145857366+MayaLIsitskaya@users.noreply.github.com>
2023-12-22 22:05:52 +0300

    fixed styles issues

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,6 +1,7 @@
-import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
 const swiper = new Swiper('.swiper', {
+
     direction: 'horizontal',
     loop: true,
     scrollbar: {
