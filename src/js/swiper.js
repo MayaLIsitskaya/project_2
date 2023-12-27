a801690d61ac2804b90daa18986a9fbd2126e662
Maya Lisitskaya <145857366+MayaLIsitskaya@users.noreply.github.com>
2023-12-27 21:13:31 +0300

    changed section styles and added some more html elements

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,11 +1,10 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
 const swiper = new Swiper('.swiper', {
-
-    direction: 'horizontal',
     loop: true,
     scrollbar: {
         el: '.swiper-scrollbar',
+        dragClass: 'swiper-scrollbar-custom-drag',
     },
 });
 
